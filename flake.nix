437fafe3597cee43c348fb2484374caef02f6caa
ScottCowe <62939710+ScottCowe@users.noreply.github.com>
2024-07-14 18:46:56 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       hp-laptop = (import ./lib { inherit inputs; }).host.mkHost {
         system = "x86_64-linux";
         hostname = "hp-laptop";
-        stateVersion = "23.11";
+        stateVersion = "24.11";
         users = [{
           name = "cowe";
           groups = [ "wheel" "networkmanager" ];
