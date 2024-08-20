149252b504043fcb018c908c1f26bfba9fe311ab
Scott Cowe <scott.t.cowe@gmail.com>
2024-08-20 22:31:29 +0100

    removed desktop config

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
       utils = (import ./lib { inherit inputs; });
     in {
       hp-laptop = utils.mkPCHost (import ./systems/hp-laptop { inherit inputs; });
-      desktop = utils.mkPCHost (import ./systems/desktop { inherit inputs; });
       iso-x86-64 = utils.mkISOSystem "x86_64-linux" inputs.nixpkgs;
       home-nas = utils.mkServerHost (import ./systems/home-nas { inherit inputs; });
       framework = utils.mkPCHost (import ./systems/framework  { inherit inputs; });
