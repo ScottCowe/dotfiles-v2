7467118fdc06006b55d92ce3338e2ed059143465
Scott Cowe <scott.t.cowe@gmail.com>
2024-08-25 17:35:15 +0100

    updated ssh config to allow pushing to github (something glitched)

--- home/modules/ssh/default.nix
+++ home/modules/ssh/default.nix
@@ -9,6 +9,12 @@ with lib; {
     programs.ssh = {
       enable = true;
       addKeysToAgent = "yes";
+
+      extraConfig = ''
+        Host github.com
+          Hostname ssh.github.com
+          Port 443
+      '';
     };
 
     services.ssh-agent.enable = true;
