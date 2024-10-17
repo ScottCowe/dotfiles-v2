ce063d714fc94dadc0b1d7245a1ce59509b75e49
Scott Cowe <scott.t.cowe@gmail.com>
2024-10-17 16:09:49 +0100

    fixed problem with waybar config

--- home/users/test/modules/waybar/default.nix
+++ home/users/test/modules/waybar/default.nix
@@ -3,19 +3,19 @@
     enable = true;
 
     settings = {
-      "layer" = "top";
-      "modules-left" = ["sway/workspaces" "sway/mode"];
-      "modules-center" = [ ];
-      "modules-right" = ["battery" "clock"];
-      "battery" = {
-        "format" = "{capacity}% {icon}";
-        "format-icons" = ["" "" "" "" ""];
-      };
-      "clock" = {
-        "format-alt" = "{:%a, %d. %b  %H:%M}";
+      bar = {
+        "layer" = "top";
+        "modules-left" = ["sway/workspaces" "sway/mode"];
+        "modules-center" = [ ];
+        "modules-right" = ["battery" "clock"];
+        "battery" = {
+          "format" = "{capacity}% {icon}";
+          "format-icons" = ["" "" "" "" ""];
+        };
+        "clock" = {
+          "format-alt" = "{:%a, %d. %b  %H:%M}";
+        };
       };
     };
-
-    style = ./style.css;
   };
 }
