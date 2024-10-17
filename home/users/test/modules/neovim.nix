61d6c5ad3564ee1768afef9072e994250a74f42c
Scott Cowe <scott.t.cowe@gmail.com>
2024-10-17 16:19:41 +0100

    fixed colorscheme for neovim

--- home/users/test/modules/neovim.nix
+++ home/users/test/modules/neovim.nix
@@ -1,8 +1,13 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, config, ... }:
 
 {
   home.packages = [ 
-    (inputs.nixvim-flake.packages."${pkgs.system}".default.extend { })
+    (inputs.nixvim-flake.packages."${pkgs.system}".default.extend {
+      colorschemes.base16 = {
+        enable = true;
+        colorscheme = config.stylix.base16Scheme;
+      };
+    })
   ];
 
   home.sessionVariables.EDITOR = "nvim";
