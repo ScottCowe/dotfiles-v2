c0bc3319ea3034f0a8bb4128737a3cf04ba41671
Scott Cowe <scott.t.cowe@gmail.com>
2024-10-17 16:06:32 +0100

    added kanagawa theme to stylix config for hp

--- home/users/test/modules/stylix/default.nix
+++ home/users/test/modules/stylix/default.nix
@@ -4,6 +4,7 @@
   stylix.enable = true;
   stylix.image = ./wallpaper.jpg;
   stylix.polarity = "dark";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
 
   stylix.fonts = {
     serif = {
