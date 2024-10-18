b1b58b6b506bea2646a8f7c93165c4a12cdf5670
Scott Cowe <scott.t.cowe@gmail.com>
2024-10-18 19:01:13 +0100

    added disko config and updated waybar config

--- home/users/test/modules/waybar/default.nix
+++ home/users/test/modules/waybar/default.nix
@@ -5,15 +5,42 @@
     settings = {
       bar = {
         "layer" = "top";
-        "modules-left" = ["sway/workspaces" "sway/mode"];
-        "modules-center" = [ ];
-        "modules-right" = ["battery" "clock"];
+        "modules-left" = [ "hyprland/workspaces" ];
+        "modules-center" = [ "clock" ];
+        "modules-right" = [ "battery" ];
         "battery" = {
-          "format" = "{capacity}% {icon}";
-          "format-icons" = ["" "" "" "" ""];
+          format = "{capacity}% {icon}";
+          format-icons = {
+            charging = [ "󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅" ];
+            default = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          };
+          format-full = "";
+          interval = 5;
+          states = {
+            warning = 20;
+            critical = 10;
+          };
+          tooltip = false;
         };
         "clock" = {
-          "format-alt" = "{:%a, %d. %b  %H:%M}";
+          "format" = "  {:%a %e %b %H:%M}";
+          "tooltip-format" = "<tt>{calendar}</tt>";
+          "calendar" = {
+            "mode" = "month";
+            "format" = {
+              "months" = "<span color='#cdd6f4'><b>{}</b></span>";
+              "days" = "<span color='#cdd6f4'><b>{}</b></span>";
+              "weeks" = "<span color='#cdd6f4'><b>W{}</b></span>";
+              "weekdays" = "<span color='#cdd6f4'><b>{}</b></span>";
+              "today" = "<span color='#f38ba8'><b>{}</b></span>";
+            };
+          };
+        };
+        "hyprland/workspaces" = {
+          "all-outputs" = true;
+          "format" = "{name}";
+          "sort-by-number" = true;
+          "active-only" = false;
         };
       };
     };
