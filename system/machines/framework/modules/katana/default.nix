d8a4b730298c6885abe2bf6472528a6932a9d413
Scott Cowe <scott.t.cowe@gmail.com>
2025-01-04 23:23:14 +0000

    added homerow mods

--- system/machines/framework/modules/katana/default.nix
+++ system/machines/framework/modules/katana/default.nix
@@ -12,8 +12,17 @@
           lsft <    z    x    c    v    b    n    m    ,    .    /    rsft
           lctl lmet lalt           spc            ralt rctl
         )
-
+ 
         (deflayer english
+          •
+          grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+          tab  q    w    e    r    t    y    u    i    o    p    [    ]    
+          @l1  @a   @s   @d   @f   g    @h   @j   @k   @l    ;    '    \    ret
+          lsft @tld z    x    c    v    b    n    m    ,    .    /    rsft
+          lctl lmet lalt           spc            ralt rctl
+        )
+
+        (deflayer gaming 
           •
           grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
           tab  q    w    e    r    t    y    u    i    o    p    [    ]    
@@ -25,7 +34,7 @@
         (deflayer layer1 
           •
           •    f1   f2   f3   f4   f5   f6   f7   f8   f9   f10  •    •    • 
-          •    @EN  •    •    •    •    •    •    •    •    •    •    •
+          @GM  @EN  •    •    •    •    •    •    •    •    •    •    •
           •    •    •    •    •    •    lft  down up   rght •    •    •    • 
           lsft •    •    •    •    •    •    •    •    •    •    •    rsft 
           lctl lmet lalt           •              ralt rctl
@@ -36,8 +45,18 @@
 
           l1 (tap-hold 200 200 esc (layer-toggle layer1))
 
+          a (tap-hold 200 200 a lalt)
+          s (tap-hold 200 200 s lctl)
+          d (tap-hold 200 200 d lmet)
+          f (tap-hold 200 200 f lsft)
+          
+          h (tap-hold 200 200 h lsft)
+          j (tap-hold 200 200 j lmet)
+          k (tap-hold 200 200 k lctl)
+          l (tap-hold 200 200 l lalt)
+
           EN (layer-switch english)
-          DE (layer-switch deutsch)
+          GM (layer-switch gaming)
         )
       '';
     };
