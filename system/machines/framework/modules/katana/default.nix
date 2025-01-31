b16fba436205423f3564b7cb5ce4c8813f0c3ad1
Scott Cowe <scott.t.cowe@gmail.com>
2025-01-31 18:01:07 +0000

    added more to kanata config

--- system/machines/framework/modules/katana/default.nix
+++ system/machines/framework/modules/katana/default.nix
@@ -19,7 +19,7 @@
           tab  q    w    e    r    t    y    u    i    o    p    [    ]    
           @l1  a    s    d    f    g    h    j    k    l    ;    '    \    ret
           lsft @tld z    x    c    v    b    n    m    ,    .    /    rsft
-          lctl lmet lalt           spc            ralt rctl
+          lctl lmet lalt           spc            @l2  rctl
         )
         
         (deflayer english-hrm
@@ -40,10 +40,20 @@
           lctl lmet lalt           •              ralt rctl
         )
 
+        (deflayer layer2 
+          •
+          •    •    •    grv  •    •    •    •    •    •    •    •    •    •
+          •    [    ]    ~    +    =    -    _    |    {    }    \    •
+          •    1    2    3    4    5    6    7    8    9    0    /    •    •
+          •    !    @    #    $    %    ^    &    *    (    )    ?    •
+          •    •    •              •              •    •
+        )
+
         (defalias
           tld S-grv
 
           l1 (tap-hold 200 200 esc (layer-toggle layer1))
+          l2 (layer-toggle layer2)
 
           a (tap-hold 200 200 a lalt)
           s (tap-hold 200 200 s lctl)
